01ec1fac226666e259e9c4dd0dace7fbbebd3bf9
Klamkin, Michael <mklamkin3@gatech.edu>
2024-11-18 01:52:06 -0500

    add docstring to ed

--- ml4opf/formulations/ed/model.py
+++ ml4opf/formulations/ed/model.py
@@ -18,6 +18,33 @@ class EDModel(OPFModel):
     def evaluate_model(
         self, reduction: Optional[str] = None, inner_reduction: Optional[str] = None
     ) -> dict[str, Tensor]:
+        """Evaluate the model on the test data.
+
+        Args:
+            reduction (str, optional): Reduction method for the metrics. Defaults to None. Must be one of "mean", "sum","max", "none".
+                                        If specified, each value in the returned dictionary will be a scalar. Otherwise, they are arrays of shape (n_test_samples,)
+
+            inner_reduction (str, optional): Reduction method for turning metrics calculated per component to per sample. Defaults to None. Must be one of "mean", "sum","max", "none".
+
+
+        Returns:
+            dict[str, Tensor]: Dictionary containing Tensor metrics of the model's performance.
+
+                `pg_lower`: Generator lower bound violation.
+
+                `pg_upper`: Generator upper bound violation.
+
+                `pf_lower`: Branch power flow lower bound violation.
+
+                `pf_upper`: Branch power flow upper bound violation.
+
+                `p_balance`: Power balance violation.
+
+                `pg_mae`: Mean absolute error of the real power generation.
+
+                `obj_mape`: Mean absolute percent error of the objective value.
+
+        """
         test_pd = self.problem.test_data["input/pd"]
 
         test_pg = self.problem.test_data["primal/pg"]
